874026bdc0707beb9817c7a3a14da9126babc191
Jibé Barth <barth.jib@gmail.com>
2020-01-19 10:03:53 +0100

    Test with ugly code

---
+++ src/UglyCode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Foo;
+
+class UglyCode {
+
+    private $_underscored_Variable;
+
+    public function waw_ugly_function_name($Badvariable = null)
+    {
+        return array();
+    }
+}
